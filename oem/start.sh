83f4179abc1b3ad755d0ce70fe99bf05b6c1e084
A.I <ailis@paw.zone>
2015-02-28 04:04:12 +0900

    Add comments

--- oem/start.sh
+++ oem/start.sh
@@ -13,6 +13,7 @@ if [ ! -e $HOME_DIR/.ssh/authorized_keys ]; then
   chmod 0600 $HOME_DIR/.ssh/authorized_keys
 fi
 
+# Set Vagrant insecure private key at the first time
 if ! grep -q "vagrant" $HOME_DIR/.ssh/authorized_keys; then
   cat <<KEY > $HOME_DIR/.ssh/authorized_keys
 ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key
@@ -21,6 +22,7 @@ fi
 
 chown -R $USERNAME:$USERNAME $HOME_DIR/.ssh
 
+# Get Docker client certificates at the first time
 if [ ! -d $HOME_DIR/.certs ]; then
   mkdir -p $HOME_DIR/.certs
 
