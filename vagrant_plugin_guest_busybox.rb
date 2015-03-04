3424b1fdfd4bfe50a21dfc94cc40d061a1ae083f
A.I <ailis@paw.zone>
2015-03-04 21:34:51 +0900

    Mount in the userdocker container as well,
    because the userdocker container is the real host of user containers.

--- vagrant_plugin_guest_busybox.rb
+++ vagrant_plugin_guest_busybox.rb
@@ -109,3 +109,58 @@ module VagrantPlugins
     end
   end
 end
+
+# Mount in the userdocker container as well
+require Vagrant.source_root.join("plugins/guests/linux/cap/mount_nfs.rb")
+module VagrantPlugins
+  module GuestLinux
+    module Cap
+      class MountNFS
+        extend Vagrant::Util::Retryable
+
+        def self.mount_nfs_folder(machine, ip, folders)
+          folders.each do |name, opts|
+            # Expand the guest path so we can handle things like "~/vagrant"
+            expanded_guest_path = machine.guest.capability(
+              :shell_expand_guest_path, opts[:guestpath])
+
+            # Do the actual creating and mounting
+            machine.communicate.sudo("mkdir -p #{expanded_guest_path}")
+
+            # Mount
+            hostpath = opts[:hostpath].dup
+            hostpath.gsub!("'", "'\\\\''")
+
+            # Figure out any options
+            mount_opts = ["vers=#{opts[:nfs_version]}"]
+            mount_opts << "udp" if opts[:nfs_udp]
+            if opts[:mount_options]
+              mount_opts = opts[:mount_options].dup
+            end
+
+            mount_command = "mount -o '#{mount_opts.join(",")}' #{ip}:'#{hostpath}' #{expanded_guest_path}"
+            retryable(on: Vagrant::Errors::LinuxNFSMountFailed, tries: 8, sleep: 3) do
+              machine.communicate.sudo(mount_command,
+                                       error_class: Vagrant::Errors::LinuxNFSMountFailed)
+            end
+
+            # Do the actual creating and mounting
+            machine.communicate.sudo("system-docker exec userdocker mkdir -p #{expanded_guest_path}")
+
+            mount_command = "system-docker exec userdocker mount -o '#{mount_opts.join(",")}' #{ip}:'#{hostpath}' #{expanded_guest_path}"
+            retryable(on: Vagrant::Errors::LinuxNFSMountFailed, tries: 8, sleep: 3) do
+              machine.communicate.sudo(mount_command,
+                                       error_class: Vagrant::Errors::LinuxNFSMountFailed)
+            end
+
+            # Emit an upstart event if we can
+            if machine.communicate.test("test -x /sbin/initctl")
+              machine.communicate.sudo(
+                "/sbin/initctl emit --no-wait vagrant-mounted MOUNTPOINT=#{expanded_guest_path}")
+            end
+          end
+        end
+      end
+    end
+  end
+end
